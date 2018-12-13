d12197991503ad1d7bb244c6e507fd14010a1b38
Locksfree <locksfree@users.noreply.github.com>
2018-12-13 21:47:31 +0000

    Update main.go

--- 2018/day1/part1/go/main.go
+++ 2018/day1/part1/go/main.go
@@ -10,20 +10,30 @@ import (
 )
 
 func main() {
-	lines := readLines()
+	values := readInts(readLines("input.txt"))
 	var result int
-	for _, line := range lines {
-		v, err := strconv.Atoi(line)
-		if err != nil {
-			log.Fatalf("Could not convert %s to an int", line)
-		}
+	for _, v := range values {
 		result += v
 	}
 	fmt.Printf("RESULT: %d\n", result)
 }
 
-func readLines() []string {
-	file, err := os.Open("input.txt")
+func readInts(entries []string) []int {
+	var res []int
+	for _, e := range entries {
+		e = strings.Replace(e, "\n", "", -1)
+		i, err := strconv.Atoi(e)
+		if err != nil {
+			log.Fatalf("Could not convert %s to an int: %v", e, err)
+		}
+		res = append(res, i)
+	}
+
+	return res
+}
+
+func readLines(name string) []string {
+	file, err := os.Open(name)
 	if err != nil {
 		log.Fatalf("Could not open input file")
 	}
